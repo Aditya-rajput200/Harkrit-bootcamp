362aa649caaa75f498d6f8f88c5807ecf08aaf55
Aditya <adityarajputsw2020@gmail.com>
2025-02-11 15:47:25 +0530

    fibo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
     let ans = is_even(15);
-    println!("{}",ans);
+    let fibNumber = fib(6);
+    println!("{}",fibNumber);
 }
 
-fn is_even(num:i32) ->bool {
+
+
+fn is_even(num:i32) ->bool { 
+
+
 
     if num % 2 ==0 {
         return  true;
@@ -11,5 +16,30 @@ fn is_even(num:i32) ->bool {
     else {
         return  false;
     }
+
+}
+// 0 1 1 2  3  5 
+// solving the fibo serires
+fn fib(num:i64) -> u64{
+     let  mut first = 0;
+     let  mut second = 1;
+   
+    if num == 0 {
+        return first
+    }
+
+    if num == 1 {
+        return  second;
+    }
+
+    for _   in 1..num -1 {
+        let  temp = second;
+        second = second + first;
+        first = temp;
+
+
+        
+    }
+    return  second;
 
 }
